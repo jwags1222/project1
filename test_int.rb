beb2a52e5853dc6f1c43b645ae7026c82fba5619
Jeremy Wagner <jtwagner1@gmail.com>
2014-07-08 14:49:31 -0400

    first commit

---
+++ test_int.rb
@@ -0,0 +1,13 @@
+#Tests integer
+puts "Input integer"
+
+if (a = gets.to_i) <= 50 
+	puts "Less than 50"
+
+elsif ((a > 50) && (a < 100)) 
+	puts "Between 50 and 100"
+
+else 
+	puts "greater than 100"
+
+end 
